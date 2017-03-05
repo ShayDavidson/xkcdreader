024c850932dabfc78d0110509334f90a28b08b93
Shay Davidson <sdavidson@paypal.com>
2017-03-05 10:38:04 +0200

    Initial project commit

---
+++ xkcdread/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xkcdread
+//
+//  Created by Davidson, Shay on 05/03/2017.
+//  Copyright (c) 2017 Shay Davidson. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
